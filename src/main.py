de1319108574bea1f2603219798675cc6c184f35
magiczny_kacpur <kafejek@gmail.com>
2025-03-05 19:20:50 +0100

    finalize generating recursivly from content

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 import os
 import shutil
 
-from extract_markdown import extract_markdown_links
-from markdowntohtml import generate_page
-from textnode import TextNode, TextType
+from markdowntohtml import generate_pages_recursive
 
 
 def copy_static_content_recursive(from_path, to_path):
@@ -38,45 +36,9 @@ def copy_static_content_recursive(from_path, to_path):
             copy_static_content_recursive(src_path, target_path)
 
 
-def split_nodes_link(old_nodes):
-    new_nodes = []
-
-    for node in old_nodes:
-        matches = extract_markdown_links(node.text)
-
-        if len(matches) == 0:
-            new_nodes.append(node)
-        else:
-            new_nodes.extend(
-                split_by_multiple_matches(matches, node.text, TextType.LINK)
-            )
-
-    return new_nodes
-
-
-def split_by_multiple_matches(matches, text, text_type):
-    nodes = []
-
-    for i in range(len(matches)):
-        splitter = ""
-        if text_type == TextType.LINK:
-            splitter = f"[{matches[i][0]}]({matches[i][1]})"
-        else:
-            splitter = f"![{matches[i][0]}]({matches[i][1]})"
-        
-        chunks = text.split(splitter)
-        nodes.extend([TextNode(chunks[0], TextType.TEXT), TextNode(matches[i][0], text_type, matches[i][1])])
-
-        text = text.split(f"{chunks[0]}{splitter}")[1]
-        if i == len(matches) - 1 and len(text) > 0:
-            nodes.append(TextNode(text, TextType.TEXT))
-
-    return nodes
-
-
 def main():
     copy_static_content_recursive("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 if __name__ == "__main__":
--- src/markdowntohtml.py
+++ src/markdowntohtml.py
@@ -1,8 +1,21 @@
+import os
 from blocks import BlockType, block_to_block_type, markdown_to_blocks
 from extract_markdown import extract_title, text_to_text_nodes
 from parentnode import ParentNode
 from textnode import TextNode, TextType, text_node_to_html_node
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dir_list = os.listdir(dir_path_content)
+    
+    for item in dir_list:
+        source_path = os.path.join(dir_path_content, item)
+        target_path = os.path.join(dest_dir_path, item)
+
+        if os.path.isfile(source_path):
+            generate_page(source_path, template_path, target_path.replace(".md", ".html"))
+        else:
+            os.mkdir(target_path)
+            generate_pages_recursive(source_path, template_path, target_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page {from_path} to {dest_path} using {template_path}")
